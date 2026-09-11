d2b8b1eab3831cf74918974a78796dae1d593162
thestrengthframework-commits <thestrengthframework@gmail.com>
2026-09-11 09:14:57 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,27 @@
-const CACHE_VERSION = 'hyrox-timer-v3';
-const CORE = ['./', './index.html', './manifest.json', './hyrox-timer-icon.png'];
+const CACHE="strength-framework-race-timer-v5";
+const ASSETS=[
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./hyrox-timer-icon.png"
+];
 
-self.addEventListener('install', event => {
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener("activate",event=>{
   event.waitUntil(
-    caches.open(CACHE_VERSION)
-      .then(cache => cache.addAll(CORE))
-      .then(() => self.skipWaiting())
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(
-        keys.filter(k => k !== CACHE_VERSION).map(k => caches.delete(k))
-      ))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-
+self.addEventListener("fetch",event=>{
   event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_VERSION).then(cache => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
+    caches.match(event.request).then(cached=>cached || fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      return response;
+    }))
   );
 });
